00e8197d5a9d989147d16a690ba10e8f5915cd36
edmund <edmundyu2008@gmail.com>
2024-06-08 19:47:56 +1200

    main menu

---
+++ MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+	
+
+	
+
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://settings.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
