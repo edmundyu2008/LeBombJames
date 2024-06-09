196b8d55f218c75dd0783bc1b281b5b8171ee911
edmundyu2008 <eyu@students.mags.school.nz>
2024-06-10 09:39:58 +1200

    movement22:41

---
+++ components/move_input_component.gd
@@ -0,0 +1,8 @@
+class_name MoveInputComponent
+extends Node
+
+@export var move_component: MoveComponent
+
+func _input(event: InputEvent) -> void:
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	move_component.velocity = Vector2(input_axis * 100, 0)
