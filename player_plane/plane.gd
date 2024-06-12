871f05bd186d636f7c605fcfea44cf078fdb7686
edmundyu2008 <eyu@students.mags.school.nz>
2024-06-13 11:09:53 +1200

    pt5,00:00

--- player_plane/plane.gd
+++ player_plane/plane.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
 @onready var muzzle: Marker2D = $Muzzle
-@onready var spawner_component: Node2D = $SpawnerComponent
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
+@onready var fire_rate_timer: Timer = $FireRateTimer
+
+func _ready() -> void:
+	fire_rate_timer.timeout.connect(fire_lasers)
+	pass
+	
+func fire_lasers() -> void:
+	spawner_component.spawn(muzzle.global_position)
---
+++ projectiles/laser.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
+func _ready() -> void:
+	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
