0b5e033ca0224f915273f61724be5b564f09f555
edmund <edmundyu2008@gmail.com>
2024-06-13 19:44:28 +1200

    7-6:45 hitboxnotworking

---
+++ opps/opps_dallas.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@onready var stats_component: StatsComponent = $StatsComponent as StatsComponent
+@onready var move_component: MoveComponent = $MoveComponent as MoveComponent
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent as HitboxComponent
+
+func _ready() -> void: 
+	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(func():
+		queue_free()
+		)
