b8fe8349e9d2c241572d3cfdd14ae7eeb508b56e
edmund <edmundyu2008@gmail.com>
2024-06-19 18:42:35 +1200

    9999999999999

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -10,7 +10,8 @@ func _on_texture_button_pressed():
 
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://settings.tscn")
+	get_tree().change_scene_to_file("res://UI/settings.tscn")
+
 
 
 func _on_exit_button_pressed():
--- UI/settings.gd
+++ UI/settings.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 
 func _on_back_button_settings_pressed():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
