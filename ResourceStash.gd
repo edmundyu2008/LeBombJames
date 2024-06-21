49e415273cd7cfbe079587ea5577d5a0d96e0a05
edmundyu2008 <eyu@students.mags.school.nz>
2024-06-21 12:28:06 +1200

    p15,10:15

--- ResourceStash.gd
+++ ResourceStash.gd
@@ -1,11 +1,5 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var game_stats: GameStats = load("res://game_stats.tres")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
--- opps/opp_generator.gd
+++ opps/opp_generator.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 @export var EnemyDallasScene: PackedScene
+@export var Minibosses_dallasScene: PackedScene
 
 var margin = 8
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 
 @onready var spawner_component: Node2D = $SpawnerComponent as SpawnerComponent
 @onready var enemy_spawn_timer: Timer = $EnemySpawnTimer
-
+@onready var miniboss_spawn_timer: Timer = $MinibossSpawnTimer
 func _ready() -> void:
 	enemy_spawn_timer.timeout.connect(handle_spawn.bind(EnemyDallasScene, enemy_spawn_timer))
+	miniboss_spawn_timer.timeout.connect(handle_spawn.bind(Minibosses_dallasScene, miniboss_spawn_timer))
 	
 	
 func handle_spawn(enemy_scene: PackedScene, timer: Timer) -> void:
