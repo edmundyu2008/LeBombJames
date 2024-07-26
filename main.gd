c649ea319579356a62e9f12caa787eb3a2b0159b
edmundyu2008 <eyu@students.mags.school.nz>
2024-07-26 12:29:46 +1200

    hearts almost done

--- main.gd
+++ main.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 @export var game_stats: GameStats
 
+@onready var hearts: TextureRect = $Hearts
 @onready var plane: Node2D = $Plane
 @onready var score_label: Label = $ScoreLabel
 @onready var health_label: Label = $HealthLabel
+
+var heart_size = 254
 
 func _ready() -> void:
 	randomize()
@@ -17,11 +20,14 @@ func _ready() -> void:
 		get_tree().change_scene_to_file("res://UI/you_died.tscn")
 		)
 	
-func update_score_label(new_score: int) -> void:
-	score_label.text = "Score: " + str(new_score)
 
 func update_health_label(new_player_health: int) -> void:
 	health_label.text = "Health: " + str(new_player_health)
+	$Hearts.size.x = new_player_health * heart_size
+	
+func update_score_label(new_score: int) -> void:
+	score_label.text = "Score: " + str(new_score)
+
 
 
 
