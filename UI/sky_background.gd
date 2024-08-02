08e509a9c1dbb53f9fa9c7230e32039cb70489e5
edmundyu2008 <eyu@students.mags.school.nz>
2024-08-02 12:28:45 +1200

    sort sound and animations

---
+++ UI/sky_background.gd
@@ -0,0 +1,10 @@
+extends ParallaxBackground
+
+@onready var background = %Background
+@onready var far_clouds = %FarClouds
+@onready var close_clouds = %CloseClouds
+
+func _process(delta: float) -> void:
+	background.motion_offset.y += 2 * delta
+	far_clouds.motion_offset.y += 10 * delta
+	close_clouds.motion_offset.y += 20 * delta
--- main.gd
+++ main.gd
@@ -5,7 +5,6 @@ extends Node2D
 @onready var hearts: TextureRect = $Hearts
 @onready var plane: Node2D = $Plane
 @onready var score_label: Label = $ScoreLabel
-@onready var health_label: Label = $HealthLabel
 
 var heart_size = 254
 
@@ -13,16 +12,15 @@ func _ready() -> void:
 	randomize()
 	update_score_label(game_stats.score)
 	game_stats.score_changed.connect(update_score_label)
-	update_health_label(game_stats.player_health)
-	game_stats.player_health_changed.connect(update_health_label)
+	update_health_points(game_stats.player_health)
+	game_stats.player_health_changed.connect(update_health_points)
 	plane.tree_exiting.connect(func():
 		await get_tree().create_timer(0.5).timeout
 		get_tree().change_scene_to_file("res://UI/you_died.tscn")
 		)
 	
 
-func update_health_label(new_player_health: int) -> void:
-	health_label.text = "Health: " + str(new_player_health)
+func update_health_points(new_player_health: int) -> void:
 	$Hearts.size.x = new_player_health * heart_size
 	
 func update_score_label(new_score: int) -> void:
