44c72eb39c127b2e12a4494a909570f8ceef596c
edmundyu2008 <eyu@students.mags.school.nz>
2024-09-03 13:32:40 +1200

    abcd21u3109389013

--- PauseMenu.gd
+++ PauseMenu.gd
@@ -1,11 +1,9 @@
 extends Control
 @export var game_stats: GameStats
 
-
 func resume():
 	self.hide()
 	get_tree().paused = false
-	
 	
 func pause():
 	self.show()
@@ -17,19 +15,14 @@ func testEsc():
 	elif Input.is_action_just_pressed("escape") and get_tree().paused:
 		resume()
 
-		
-
-
 func _on_resume_pressed():
 	resume()
-
 
 func _on_restart_pressed():
 	resume()
 	get_tree().change_scene_to_file("res://UI/countdown.tscn")
 	game_stats.score = 0
 	game_stats.player_health = 3
-
 
 func _on_exit_pressed():
 	get_tree().paused = false 
--- components/move_input_component.gd
+++ components/move_input_component.gd
@@ -7,5 +7,11 @@ extends Node
 
 func _input(_event: InputEvent) -> void:
 	var input_direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	if Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left"):
+		input_direction.y = 0
+	elif Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down"):
+		input_direction.x = 0
+	else:
+		input_direction = Vector2.ZERO
 	move_component.velocity = input_direction * move_stats.speed
 
